237d034f4115d8151f5383293f2c3c3adb08675c
Sayed Rakinur Rahman <158179923+RakinUrRahman@users.noreply.github.com>
2024-04-15 22:19:35 +0600

    Add files via upload

---
+++ src/rakin/LoanApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rakin;
+
+/**
+ *
+ * @author rakin
+ */
+class LoanApplication {
+
+    LoanApplication(String selectedItem, int parseInt, String selectedItem1, String text) {
+    }
+    
+}
